4e1d5f37ebbef05e2bebb02a749c3522a8b6a5a2
Emanuel <=>
2025-09-22 04:33:30 +0100

    feat: Iniciar categorias

---
+++ src/Services/Products/Categories/get-product-category.service.ts
@@ -0,0 +1,32 @@
+import { PrismaProductsCategories } from "../../../Repositories/Products/Categories/Prisma/PrismaProductsCategories";
+
+class GetProductsCategoryDatas
+{
+    constructor(private readonly repository: PrismaProductsCategories){}
+
+    async getCategory(id_category: number)
+    {
+        try
+        {
+            if(!id_category)
+            {
+                return {success: false, statusCode: 400, message:"Informe a categoria"}
+            }
+            if(!await this.repository.getCategoryData({action:"GetOnlyBasicsDatas"},id_category, undefined))
+            {
+                return {success: false, statusCode: 404, message:"Categoria não encontrada"}
+            }
+            const result = await this.repository.getCategoryData({action:"getAll"}, id_category, undefined)
+            if(!result)
+            {
+                return {success: false, statusCode: 500, message:"Ocorreu um erro ao encontrar esta categoria, tente novamente"}
+            }
+            return {success: true, statusCode: 200, datas: result}
+        } catch (error: any)
+        {
+            console.log(error)
+            return {success: false, statusCode: 500, message: "Ocorreu um erro interno, tente novamente!"}     
+        }
+    }
+}
+export {GetProductsCategoryDatas}
---
+++ src/Services/Products/Categories/register-product-category.service.ts
@@ -0,0 +1,34 @@
+import { PrismaProductsCategories } from "../../../Repositories/Products/Categories/Prisma/PrismaProductsCategories";
+import { productsCategoriesDatas } from "../../../interfaces/Products/Categories/interface";
+import { PrismaClient } from "../../../../generated/prisma";
+
+class RegisterProductCategoryService
+{
+    constructor(private readonly repository: PrismaProductsCategories){}
+
+    async register(datas: productsCategoriesDatas)
+    {
+        try
+        {
+            if(!datas.name || !datas.description)
+            {
+                return {success: false, statusCode: 400, message:"Preencha todos os campos"}
+            }
+            if(await this.repository.getCategoryData({action:"GetOnlyBasicsDatas"}, undefined, datas.name))
+            {
+                return {success: false, statusCode: 409, message:"Já existe uma categoria com este nome"}
+            }
+            const result = await this.repository.register(datas)
+            if(!result)
+            {
+                return {success: false, statusCode: 400, message:"Ocorreu um erro ao cadastrar esta categoria"} 
+            }
+            return {success: true, statusCode: 201, message:"Categoria criada com sucesso"}
+        } catch (error: any)
+        {
+            console.log(error)
+            return {success: false, statusCode: 500, message: "Ocorreu um erro interno, tente novamente!"}             
+        }
+    }
+}
+export {RegisterProductCategoryService}
