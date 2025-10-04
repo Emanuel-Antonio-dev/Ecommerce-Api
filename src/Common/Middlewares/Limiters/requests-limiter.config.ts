49735c80de2ca9f060f2581aa404fc27f5d5472d
Emanuel <=>
2025-10-04 23:28:14 +0100

    refactor(limiterConfig): adicionar legacyHeaders e standardHeaders

--- src/Common/Middlewares/Limiters/requests-limiter.config.ts
+++ src/Common/Middlewares/Limiters/requests-limiter.config.ts
@@ -4,7 +4,9 @@ const limiterConfig = (message: string, time: number = 2) =>{
     return limitrer({
     windowMs: time * 60 * 1000,
     max: 4,
-    message: message
+    message: message,
+    legacyHeaders: false,
+    standardHeaders: true
 })
 }
 export{limiterConfig}
