db036947683efcdc0c4e57b0e045d2a4feae066e
Emanuel <=>
2025-10-09 21:54:40 +0100

    feat: Enhance authentication tests and add logging for user actions

--- src/__tests__/Authentications/local-signin.test.ts
+++ src/__tests__/Authentications/local-signin.test.ts
@@ -1,22 +1,21 @@
 import request from "supertest";
 import { app } from "../../app";
-import { resetDatabase, prismaInstace } from "../../../tests/setup/prismaInstace";
+import { resetDatabase } from "../../../tests/setup/prismaInstace";
 
-describe("Auth › /local-signin", () => {
-  const user = {
+const user = {
     first_name: "Emanuel",
     last_name: "Paulo",
     email: "emaricaroffice@gmail.com",
     password: "Emaricar16@",
     contacts: [{ phone_number: "944395932" }],
     addresses: [{ city: "Luanda", street: "rua da china" }],
-  };
+};
 
-  beforeAll(async () => {
+describe("Auth › /local-signin", () => {
+  beforeEach(async () => {
     await resetDatabase();
     await request(app).post("/api.ecommerce/v1/auth/signup").send(user);
   });
-
   it("Deve retornar 200 e o token se o login for válido", async () => {
     const response = await request(app)
       .post("/api.ecommerce/v1/auth/local-signin")
@@ -24,8 +23,7 @@ describe("Auth › /local-signin", () => {
         email: user.email,
         password: user.password,
       });
-
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(200); 
     expect(response.body).toHaveProperty("accessToken");
   });
 
--- src/__tests__/Authentications/register-user.test.ts
+++ src/__tests__/Authentications/register-user.test.ts
@@ -1,41 +1,37 @@
 import request from "supertest";
 import { app } from "../../app";
-import { resetDatabase, prismaInstace } from "../../../tests/setup/prismaInstace";
+import { resetDatabase } from "../../../tests/setup/prismaInstace";
+
+const userPayload = {
+  first_name: "Emanuel",
+  last_name: "Paulo",
+  email: "emaricaroffice@gmail.com",
+  password: "Emaricar16@",
+  contacts: [{ phone_number: "944395932" }],
+  addresses: [{ city: "Luanda", street: "rua da china" }],
+};
 
 describe("Auth /signup", () => {
-  beforeAll(async () => {
+  beforeEach(async () => {
     await resetDatabase();
   });
 
   it("Deve retornar 201 se o usuário for criado com sucesso", async () => {
     const response = await request(app)
       .post("/api.ecommerce/v1/auth/signup")
-      .send({
-        first_name: "Emanuel",
-        last_name: "Paulo",
-        email: "emaricaroffice@gmail.com",
-        password: "Emaricar16@",
-        contacts: [{ phone_number: "944395932" }],
-        addresses: [{ city: "Luanda", street: "rua da china" }],
-      });
+      .send(userPayload);
 
     expect(response.status).toBe(201);
     expect(response.body.success).toBeTruthy();
     expect(response.body.message).toBe("Conta criada com sucesso!");
-
   });
 
   it("Deve retornar 409 se o e-mail já existir", async () => {
+    await request(app).post("/api.ecommerce/v1/auth/signup").send(userPayload);
+
     const response = await request(app)
       .post("/api.ecommerce/v1/auth/signup")
-      .send({
-        first_name: "Emanuel",
-        last_name: "Paulo",
-        email: "emaricaroffice@gmail.com",
-        password: "Emaricar16@",
-        contacts: [{ phone_number: "944395932" }],
-        addresses: [{ city: "Luanda", street: "rua da china" }],
-      });
+      .send(userPayload);
 
     expect(response.status).toBe(409);
   });
