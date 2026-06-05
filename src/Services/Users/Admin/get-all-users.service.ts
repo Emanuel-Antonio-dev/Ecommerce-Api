d5ea6e5fe23e140e569d9906f57c20cfb9b6b43d
Emanuel <=>
2026-06-05 15:03:18 +0100

    refactor: update GetAllUsersService to use IAdminRepositories for improved abstraction

--- src/Services/Users/Admin/get-all-users.service.ts
+++ src/Services/Users/Admin/get-all-users.service.ts
@@ -1,40 +1,40 @@
-import { PrismaAdminRepositories } from "../../../Repositories/Users/Admin/Prisma/PrismaAdminRepositories";
+import { IAdminRepositories } from "../../../Repositories/Users/Admin/I-admin-repositories"
 
 class GetAllUsersService
-{
-    constructor(private readonly repository: PrismaAdminRepositories){}
-    async getAllUsers(page?: number, limit?: number)
     {
-        try
+        constructor(private readonly repository: IAdminRepositories){}
+        async getAllUsers(page?: number, limit?: number)
         {
-            const take = limit && limit > 0 ? limit : 50
-            const currentPage = page && page > 0 ? page : 1
-            const skip = (currentPage - 1) * take
-
-            const result = await this.repository.getAllUsers(take, skip)
-            if(result.length === 0)
+            try
             {
-                return {success: true, statusCode: 404, message:"De momento ainda não existem usuários"}
-            }
-            const totalProducts = await this.repository.countUsers()
-            const totalPages = Math.ceil(totalProducts / take)
-            return {
-                success: true,
-                statusCode: 200,
-                datas: result,
-                paginationDatas: {
-                    page: currentPage,
-                    limit: take,
-                    returned: result.length,
-                    totalItems: totalProducts,
-                    totalPages
+                const take = limit && limit > 0 ? limit : 50
+                const currentPage = page && page > 0 ? page : 1
+                const skip = (currentPage - 1) * take
+
+                const result = await this.repository.getAllUsers(take, skip)
+                if(result.length === 0)
+                {
+                    return {success: true, statusCode: 404, message:"De momento ainda não existem usuários"}
                 }
+                const totalProducts = await this.repository.countUsers()
+                const totalPages = Math.ceil(totalProducts / take)
+                return {
+                    success: true,
+                    statusCode: 200,
+                    datas: result,
+                    paginationDatas: {
+                        page: currentPage,
+                        limit: take,
+                        returned: result.length,
+                        totalItems: totalProducts,
+                        totalPages
+                    }
+                }
+            } catch (error: any)
+            {
+                console.log(error)
+                return {success: false, statusCode: 500, message: "Ocorreu um erro interno, tente novamente!"}
             }
-        } catch (error: any)
-        {
-            console.log(error)
-            return {success: false, statusCode: 500, message: "Ocorreu um erro interno, tente novamente!"}
         }
-    }
 }
 export{GetAllUsersService}
